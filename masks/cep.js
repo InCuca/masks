31b15817e365d105d1602eeeea165e6c3d16d489
Walker Leite <walker@incuca.com.br>
2018-04-04 19:12:01 -0300

    feat(masks): add cep

---
+++ masks/cep.js
@@ -0,0 +1,28 @@
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+import doFormat from './helpers/format';
+
+export function mask(rawValue) {
+  return [
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    '-',
+    /\d/,
+    /\d/,
+    /\d/,
+  ]
+}
+
+export function unmask(masked) {
+  const rawValue = masked.replace(
+    new RegExp(/-/),
+    ''
+  );
+  return rawValue;
+}
+
+export function format(value) {
+  return doFormat(value, mask);
+}
---
+++ masks/cep.spec.js
@@ -0,0 +1,8 @@
+import { format, mask } from './cep.js'
+describe('cep mask', () => {
+  it('should mask correctly', () => {
+    expect(
+      format('11111111', mask)
+    ).toEqual('11111-111')
+  })
+})
