e8f1ae3ccdd34396fac94c47a36ae8b91ae72fa3
Walker <walker@praiseweb.com.br>
2018-04-04 12:09:05 -0300

    fix(vue): fix vue integration logic

--- integrations/vue.js
+++ integrations/vue.js
@@ -1,40 +1,16 @@
-import * as decimalNumber from '../masks/decimalNumber';
-import * as meters from '../masks/meters';
-import * as centimeters from '../masks/centimeters';
-import * as aream3 from '../masks/aream3';
-import * as areacm3 from '../masks/areacm3';
-import * as kilograms from '../masks/kilograms';
-import * as dollars from '../masks/dollars';
+import {masks} from '../index.js';
 
 // To be used with text-mask/text-mask
 export default {
   install(Vue) {
-    Vue.prototype.$masks = {
-      decimalNumber: decimalNumber.mask,
-      meters: meters.mask,
-      centimeters: centimeters.mask,
-      aream3: aream3.mask,
-      areacm3: areacm3.mask,
-      kilograms: kilograms.mask,
-      dollars: dollars.mask,
-    };
+    Vue.prototype.$masks = {};
+    Vue.prototype.$unmask = {};
+    Vue.prototype.$format = {};
 
-    Vue.prototype.$unmask = {
-      decimalNumber: decimalNumber.unmask,
-      meters: meters.unmask,
-      centimeters: centimeters.unmask,
-      areacm3: areacm3.unmask,
-      kilograms: kilograms.unmask,
-      dollars: dollars.unmask,
-    };
-
-    Vue.prototype.$format = {
-      decimalNumber: decimalNumber.format,
-      meters: meters.format,
-      centimeters: centimeters.format,
-      areacm3: areacm3.format,
-      kilograms: kilograms.format,
-      dollars: dollars.format,
-    };
+    for (maskName in masks) {
+      Vue.prototype.$masks[maskName] = masks[maskName].mask;
+      Vue.prototype.$unmask[maskName] = masks[maskName].unmask;
+      Vue.prototype.$format[maskName] = masks[maskName].format;
+    }
   },
 };
